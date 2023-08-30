3efc01725099feccd81c4b9d7e11c429934d79ab
moxuy <jichangee@gmail.com>
2023-08-30 18:02:42 +0800

    update

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require('express');
+const cors = require('cors')
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
 const app = express();
+
+app.use(cors())
 const xintaiAdminOptions = {
   target: 'http://15366570332.gnway.cc', // target host
   changeOrigin: true, // needed for virtual hosted sites
-  ws: true, // proxy websockets
   pathRewrite: {
     '^/xintai-admin/api': '/xintai/back',
   }
